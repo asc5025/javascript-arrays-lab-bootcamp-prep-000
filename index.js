b48509161750be91a2a9189f5e00197ac4b346d0
Albert Chun <albert.s.chun@gmail.com>
2018-08-01 19:51:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your functions and code here
 kittens = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendKitten("Ralph") {
+function destructivelyAppendKitten(kittens, "name") {
   
 }
